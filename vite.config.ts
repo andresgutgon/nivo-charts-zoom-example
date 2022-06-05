46a316e986f2bdd9be6c3b159d8ccfae507dda58
andresgutgon <andresgutgon@gmail.com>
2022-06-05 13:09:00 +0200

    Local @nivo/core and @nivo/line

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,15 @@
+import path from 'path'
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  resolve: {
+    alias:{
+      'react/jsx-runtime': 'react/jsx-runtime.js',
+      '@nivo/core' : path.resolve(__dirname, '../nivo/packages/core/dist/nivo-core.es.js'),
+      '@nivo/line' : path.resolve(__dirname, '../nivo/packages/line/dist/nivo-line.es.js')
+    },
+  },
   plugins: [react()]
 })
